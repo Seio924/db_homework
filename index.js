9b73faf4bbd8609d876a4b4fa8d4a967e39fc637
Seio924 <seio9241@gmail.com>
2023-06-17 15:59:01 +0900

    All functions implemented

--- index.js
+++ index.js
@@ -33,14 +33,19 @@ router.get('/', function(req, res, next) {
   next_page = current_page+1;
   prev_page = current_page-1;
 
+  if(current_page==0){
+    prev_page=0;
+  }
+
   maria.query('SELECT count(*) as c FROM BOARD', function(err, rows, fields) {
     total = rows[0].c;
     total_page = Math.ceil(total/count_page);
 
     var sql;
-    //console.log(total, current_page*5 + 5)
+    
     if (total < current_page*5 + 5){
-      console.log("1");
+      next_page = current_page;
+      
       sql = `SELECT * FROM BOARD LIMIT ${(current_page*5).toString()},${(total).toString()}`;
       maria.query(sql, function(err, rows, fields) {
         if(!err) {
@@ -66,8 +71,8 @@ router.get('/', function(req, res, next) {
       });
     }
     else{
-      console.log("2");
       sql = `SELECT * FROM BOARD LIMIT ${(current_page*5).toString()},${(current_page*5 + 5).toString()}`;
+      
       maria.query(sql, function(err, rows, fields) {
         if(!err) {
           for(var i=0; i<count_page; i++){
@@ -95,40 +100,10 @@ router.get('/', function(req, res, next) {
 
 });
 
+
 router.get('/write_board', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-/*
-router.get('/create', function(req, res) {
-  maria.query('CREATE TABLE BOARD ('
-	+'TITLE VARCHAR(200) NOT NULL,'
-	+'NAME VARCHAR(200) NULL ,'
-  +'CONTENTS VARCHAR(200) NULL ,'
-	+'PRIMARY KEY (TITLE, NAME));', function(err, rows, fields) {
-    if(!err) {
-      res.send(rows); // responses send rows
-    } else {
-      console.log("err : " + err);
-      res.send(err);  // response send err
-    }
-  });
-});
-*/
-
-router.get('/drop', function(req, res) {
-  maria.query('DROP TABLE BOARD', function(err, rows, fields) {
-    if(!err) {
-      res.send(rows); // responses send rows
-      
-      
-    } else {
-      console.log("err : " + err);
-      res.send(err);  // response send err
-    }
-  });
-});
-
 
 
 router.post('/insert', jsonParser, function(req, res) {
@@ -153,25 +128,45 @@ router.post('/insert', jsonParser, function(req, res) {
   const dt = dateStr + ' ' + timeStr;
   const params = [title, writer, content, dt];
 
-  console.log(title);
+  
   maria.query(sql, params, function(err, rows, fields) {
     if(!err) {
-      console.log(rows);
-      //res.send(rows); // responses send rows
       res.redirect('/');
     } else {
       console.log("err : " + err);
       res.send(err);  // response send err
     }
   });
-  //res.render('board_ui', { title: 'Express' });
 });
 
-router.get('/select', function(req, res) {
-  maria.query('SELECT count(*) as c FROM BOARD', function(err, rows, fields) {
+
+router.post('/com_insert', jsonParser, function(req, res) {
+  const date = new Date();
+
+  const year = date.getFullYear();
+  const month = ('0' + (date.getMonth() + 1)).slice(-2);
+  const day = ('0' + date.getDate()).slice(-2);
+  const dateStr = year + '-' + month + '-' + day;
+
+  const hours = ('0' + date.getHours()).slice(-2);
+  const minutes = ('0' + date.getMinutes()).slice(-2);
+  const seconds = ('0' + date.getSeconds()).slice(-2);
+  const timeStr = hours + ':' + minutes + ':' + seconds;
+
+
+  const sql = 'INSERT INTO comments(c_writer, c_content, c_dt, c_num) VALUES(?,?,?,?)';
+  
+  
+  const c_writer = req.body.c_writer;
+  const c_content = req.body.c_content;
+  const c_dt = dateStr + ' ' + timeStr;
+  const c_num = req.body.c_num;
+  const params = [c_writer, c_content, c_dt, c_num];
+
+
+  maria.query(sql, params, function(err, rows, fields) {
     if(!err) {
-      console.log(rows[0].c);
-      res.send(rows); // responses send rows
+      res.redirect(`/select?num=${c_num}`);
     } else {
       console.log("err : " + err);
       res.send(err);  // response send err
@@ -179,10 +174,157 @@ router.get('/select', function(req, res) {
   });
 });
 
-router.get('/update', function(req, res) {
-  maria.query('UPDATE DEPARTMENT SET NAME="UPD ENG" WHERE DEPART_CODE=5001', function(err, rows, fields) {
+
+router.get('/select', function(req, res) {
+  if(req.query.num){
+    var re = new Array(4);
+    var n;
+
+    sql = `SELECT * FROM BOARD WHERE num=${req.query.num}`;
+    maria.query(sql, function(err, rows, fields) {
+      n = rows[0].num;
+      tmp = rows;
+      if(!err) {
+        sql = `SELECT count(*) as count FROM comments WHERE c_num=${n}`; //댓글 없을때
+        maria.query(sql, function(err, rows, fields) {
+          if(!err) {
+            if(rows[0].count==0){
+              res.render('select_board', { 
+                title: 'Express',
+                s_title: tmp[0].title,
+                s_writer: tmp[0].writer,
+                s_content: tmp[0].content,
+                s_dt: tmp[0].dt,
+                s_num: tmp[0].num,
+                count: 0
+              });
+            }
+            else{
+              sql = `SELECT * FROM comments WHERE c_num=${n}`;  //댓글 있을때
+              maria.query(sql, function(err, rows, fields) {
+                if(!err) {
+                  for (var i = 0; i < 4; i++) {
+                    re[i] = new Array(rows.length);
+                  }
+
+                  for(var i=0; i<rows.length; i++){
+                    re[0][i] = rows[i].c_writer;
+                    re[1][i] = rows[i].c_content;
+                    re[2][i] = rows[i].c_dt;
+                    re[3][i] = rows[i].com_num;
+                  }
+                  
+                  res.render('select_board', { 
+                    title: 'Express',
+                    s_title: tmp[0].title,
+                    s_writer: tmp[0].writer,
+                    s_content: tmp[0].content,
+                    s_dt: tmp[0].dt,
+                    s_num: tmp[0].num,
+                    com: re,
+                    count: rows.length
+                  });
+
+                } else {
+                  console.log("err : " + err);
+                  res.send(err);  // response send err
+                }
+              });
+            }
+          } else {
+            console.log("err : " + err);
+            res.send(err);  // response send err
+          }
+        });
+      }
+      else {
+        console.log("err : " + err);
+        res.send(err);  // response send err
+      }
+    });
+  }
+  else{
+    var t = req.query.title;
+    var w = req.query.writer;
+    var d = req.query.dt;
+    var re = new Array(4);
+    var n;
+
+    sql = `SELECT * FROM BOARD WHERE board.title='${t}' AND board.writer='${w}' AND board.dt='${d}'`;
+    maria.query(sql, function(err, rows, fields) {
+      n = rows[0].num;
+      tmp = rows;
+      if(!err) {
+        sql = `SELECT count(*) as count FROM comments WHERE c_num=${n}`; //댓글 없을때
+        maria.query(sql, function(err, rows, fields) {
+          if(!err) {
+            if(rows[0].count==0){
+              res.render('select_board', { 
+                title: 'Express',
+                s_title: tmp[0].title,
+                s_writer: tmp[0].writer,
+                s_content: tmp[0].content,
+                s_dt: tmp[0].dt,
+                s_num: tmp[0].num,
+                count: 0
+              });
+            }
+            else{
+              sql = `SELECT * FROM comments WHERE c_num=${n}`;  //댓글 있을때
+              maria.query(sql, function(err, rows, fields) {
+                if(!err) {
+                  for (var i = 0; i < 4; i++) {
+                    re[i] = new Array(rows.length);
+                  }
+
+                  for(var i=0; i<rows.length; i++){
+                    re[0][i] = rows[i].c_writer;
+                    re[1][i] = rows[i].c_content;
+                    re[2][i] = rows[i].c_dt;
+                    re[3][i] = rows[i].com_num;
+                  }
+                  
+                  res.render('select_board', { 
+                    title: 'Express',
+                    s_title: tmp[0].title,
+                    s_writer: tmp[0].writer,
+                    s_content: tmp[0].content,
+                    s_dt: tmp[0].dt,
+                    s_num: tmp[0].num,
+                    com: re,
+                    count: rows.length
+                  });
+
+                } else {
+                  console.log("err : " + err);
+                  res.send(err);  // response send err
+                }
+              });
+            }
+          } else {
+            console.log("err : " + err);
+            res.send(err);  // response send err
+          }
+        });
+      }
+      else {
+        console.log("err : " + err);
+        res.send(err);  // response send err
+      }
+    });
+  }
+  
+});
+
+
+
+router.get('/com_delete', function(req, res) {
+  var com_num = req.query.com_num;
+  var num = req.query.num;
+
+  maria.query(`DELETE FROM comments WHERE com_num=${com_num}`, function(err, rows, fields) {
     if(!err) {
-      res.send(rows); // responses send rows
+      res.redirect(`/select?num=${num}`);
     } else {
       console.log("err : " + err);
       res.send(err);  // response send err
@@ -191,9 +333,11 @@ router.get('/update', function(req, res) {
 });
 
 router.get('/delete', function(req, res) {
-  maria.query('DELETE FROM BOARD WHERE DEPART_CODE=5001', function(err, rows, fields) {
+  var num = req.query.num;
+
+  maria.query(`DELETE FROM board WHERE num=${num}`, function(err, rows, fields) {
     if(!err) {
-      res.send(rows); // responses send rows
+      res.redirect('/');
     } else {
       console.log("err : " + err);
       res.send(err);  // response send err
